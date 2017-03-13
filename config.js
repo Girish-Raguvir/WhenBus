2ce957ad007f12f8fc093f484b56b4208e612b82
Girish-Raguvir <girishraguvir@gmail.com>
2017-03-13 17:12:21 +0530

    modified mongo.js

--- config.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    'database': 'mongodb://girishraguvir:qwerty@123@ds129030.mlab.com:29030/whenbus'
-};
--- models/mongo.js
+++ models/mongo.js
@@ -1,6 +1,13 @@
 var mongoose    =   require("mongoose");
 
-mongoose.connect('mongodb://girishraguvir:qwerty@ds129030.mlab.com:29030/whenbus');
+mongoose.connect('mongodb://girishraguvir:qwerty@ds129030.mlab.com:29030/whenbus')
+
+// To be used during deployment
+
+// mongoose.connect(process.env.PROD_MONGODB, function (error) {
+//     if (error) console.error(error);
+//     else console.log('mongo connected');
+// });
 
 var mongoSchema =   mongoose.Schema;
 var user_schema  = {
