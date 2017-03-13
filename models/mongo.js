5de03605125b222f25595e23a92d1be707aba61b
Girish-Raguvir <girishraguvir@gmail.com>
2017-03-13 16:44:45 +0530

    modified mongo.js

--- models/mongo.js
+++ models/mongo.js
@@ -1,5 +1,10 @@
 var mongoose    =   require("mongoose");
 
+mongoose.connect(process.env.PROD_MONGODB, function (error) {
+    if (error) console.error(error);
+    else console.log('mongo connected');
+});
+
 var mongoSchema =   mongoose.Schema;
 var user_schema  = {
     "email" : String,
