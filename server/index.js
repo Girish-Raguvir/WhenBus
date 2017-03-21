1f2321cb348a4489efd22c17cc983e23d00e065d
Rahul Ramesh <rahul.ramesh10@yahoo.co.in>
2017-03-21 19:47:57 +0530

    Added bus timings also to DB

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
-var express     =   require("express");
-var app         =   express();
-var bodyParser  =   require("body-parser");
-var router      =   express.Router();
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+var router = express.Router();
 
 var uuid = require('node-uuid');
 var crypto = require('crypto');
@@ -10,69 +10,88 @@ var account_controller = require("./controllers/account.js")
 var User = require("./models/user.js")
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({"extended" : false}));
+app.use(bodyParser.urlencoded({
+	"extended": false
+}));
 
-router.get("/",function(req,res){
-    res.json({"error" : false,"message" : "Welcome to WhenBus!"});
+router.get("/", function(req, res) {
+	res.json({
+		"error": false,
+		"message": "Welcome to WhenBus!"
+	});
 });
 
 router.route("/users/register")
-    .post(function(req,res){
+	.post(function(req, res) {
 
-        var response = {};
+		var response = {};
 
-        var email = req.body.email;
-        var name = req.body.name;
-        var password = req.body.password;
-        var password_salt = uuid.v4();
+		var email = req.body.email;
+		var name = req.body.name;
+		var password = req.body.password;
+		var password_salt = uuid.v4();
 
-        var account = new account_controller(User,{});
+		var account = new account_controller(User, {});
 
-        account.hashPassword(password, password_salt, function(err, password_hash){
+		account.hashPassword(password, password_salt, function(err, password_hash) {
 
-          if(err) {
-              response = {"success" : false, "message" : "Error adding data"};
-              res.json(response);
-          } else {
-            var new_user = new User();
-            new_user.email = email;
-            new_user.name = name;
-            new_user.password_salt = password_salt;
-            new_user.password_hash = password_hash;
+			if (err) {
+				response = {
+					"success": false,
+					"message": "Error adding data"
+				};
+				res.json(response);
+			} else {
+				var new_user = new User();
+				new_user.email = email;
+				new_user.name = name;
+				new_user.password_salt = password_salt;
+				new_user.password_hash = password_hash;
 
-            account.register(new_user, function(err, resp){
-              if(err) {
-                  response = {"success" : false, "message" : "Error registering"};
-              } else {
-                  response = {"success" : resp.success, "message" : resp.payload};
-              }
-              res.json(response);
-            });
-          }
-      });
-    });
+				account.register(new_user, function(err, resp) {
+					if (err) {
+						response = {
+							"success": false,
+							"message": "Error registering"
+						};
+					} else {
+						response = {
+							"success": resp.success,
+							"message": resp.payload
+						};
+					}
+					res.json(response);
+				});
+			}
+		});
+	});
 
-  router.route("/users/login")
-      .post(function(req,res){
+router.route("/users/login")
+	.post(function(req, res) {
 
-          var response = {};
+		var response = {};
 
-          var email = req.body.email;
-          var password = req.body.password;
+		var email = req.body.email;
+		var password = req.body.password;
 
-          var account = new account_controller(User,{});
+		var account = new account_controller(User, {});
 
-          account.login(email, password, function(err, resp){
-            if(err) {
-                response = {"success" : false, "message" : "Error logging in"};
-            } else {
-                response = {"success" : resp.success, "message" : resp.payload};
-            }
-            res.json(response);
-          });
-      });
+		account.login(email, password, function(err, resp) {
+			if (err) {
+				response = {
+					"success": false,
+					"message": "Error logging in"
+				};
+			} else {
+				response = {
+					"success": resp.success,
+					"message": resp.payload
+				};
+			}
+			res.json(response);
+		});
+	});
 
-
-app.use('/',router);
+app.use('/', router);
 
 app.listen(process.env.PORT || 3000);
