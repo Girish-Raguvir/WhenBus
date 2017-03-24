018f2c0e3838ec359c6263aafcf92fa44b002195
seshu <seshu.nine6@gmail.com>
2017-03-24 16:37:46 +0530

    App added

---
+++ Frontend/whenbus/app/src/main/java/info/noobslab/whenbus/app/AppConfig.java
@@ -0,0 +1,9 @@
+package info.noobslab.whenbus.app;
+
+public class AppConfig {
+	// Server user login url
+	public static String URL_LOGIN = "http://192.168.0.108:3000/";
+
+	// Server user register url
+	public static String URL_REGISTER = "http://192.168.0.108:3000/users";
+}
