b8b1cd4b5a268497c6935894f0159ac00156ed87
seshu <seshu.nine6@gmail.com>
2017-03-26 10:40:02 +0530

    working one with login and registration

--- Frontend/whenbus/app/src/main/java/info/noobslab/whenbus/app/AppConfig.java
+++ Frontend/whenbus/app/src/main/java/info/noobslab/whenbus/app/AppConfig.java
@@ -2,8 +2,12 @@ package info.noobslab.whenbus.app;
 
 public class AppConfig {
 	// Server user login url
-	public static String URL_LOGIN = "http://10.22.26.64:3000/users/login";
+
+	public static String BASE_URL = "https://cs3410-whenbus.herokuapp.com/";
+	public static String URL_LOGIN = BASE_URL + "users/login";
 
 	// Server user register url
-	public static String URL_REGISTER = "http://10.22.26.64/users/register";
+	public static String URL_REGISTER = BASE_URL + "users/register";
+
+	public static String URL_POST_BUS = BASE_URL + "bus";
 }
