fa461f532e9e83aff65a2edceafb451e3abbd6ee
Rahul Ramesh <rahul13ramesh@gmail.com>
2017-04-07 11:05:34 +0530

    Pushed script for jsdon, apidoc

--- create_doc.sh
+++ create_doc.sh
@@ -1,4 +1,4 @@
-./node_modules/.bin/jsdoc server/controllers/bus_and_stop.js -d servdoc
+./node_modules/.bin/jsdoc -d servdoc -c conf.json
 
 # API DOC
 # ./node_modules/.bin/apidoc -i server/ -o apidoc
