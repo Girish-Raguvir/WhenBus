1ef4e0e83610540f45518cf50b6eebd211bd4973
Girish-Raguvir <girishraguvir@gmail.com>
2017-04-08 11:19:37 +0530

    static serving of docs

---
+++ server/static/apidoc/api_project.js
@@ -0,0 +1,16 @@
+define({
+  "name": "WhenBus",
+  "version": "0.1.0",
+  "description": "Description of various endpoints of the WhenBus backend",
+  "title": "WhenBus API's",
+  "url": "https://cs3410-whenbus.herokuapp.com",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2017-04-06T20:54:33.738Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.5"
+  }
+});
