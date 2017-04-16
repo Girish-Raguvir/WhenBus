db60d633ef6b9d1a7f5ec932e9d449ea9b3d43b4
Rahul Ramesh <rahul13ramesh@gmail.com>
2017-04-16 15:30:47 +0530

    Changed script for loading

--- load_data.sh
+++ load_data.sh
@@ -1,3 +1,7 @@
-node ./server/load-db/bus_stop_populate.js
-node ./server/load-db/bus_populate.js
-node ./server/load-db/bus_routes_populate.js
+# node ./server/load-db/bus_stop_populate.js
+# node ./server/load-db/bus_populate.js
+# node ./server/load-db/bus_routes_populate.js
+
+node ./server/load-db/bus_pop_mtc.js
+node ./server/load-db/bus_stop_pop_mtc.js
+node ./server/load-db/bus_route_pop_mtc.js
